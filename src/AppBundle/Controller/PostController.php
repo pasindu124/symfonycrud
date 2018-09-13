b1c9f3cdbb540446ba0db3a92942a54ce5756b8e
Pasindu <pasindu@012.local>
2018-09-13 17:54:22 +0530

    conect frontend

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -13,6 +13,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PostController extends Controller
 {
@@ -47,6 +49,7 @@ class PostController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($contact);
             $em->flush();
+            $this->addFlash('message','Contact Succcessfully Added!');
             return $this->redirectToRoute('view_contact_route');
 
         }
@@ -57,6 +60,24 @@ class PostController extends Controller
             'form' => $form->createView()  ]);
     }
 
+    /**
+     * @Route("/post/insertdata", name="insert_post_route")
+     */
+
+    public function insertDataAction(){
+        $foo = $_GET['emp'];
+        echo $foo;
+        //exit();
+
+        $response = new Response(json_encode("oas"));
+        $response->headers->set('Content-Type', 'application/json');
+
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT');
+        $response->headers->set('Access-Control-Allow-Headers', 'X-Header-One,X-Header-Two');
+        return $response;
+    }
+
 
     /**
      * @Route("/post/viewcontact", name="view_contact_route")
@@ -64,15 +85,126 @@ class PostController extends Controller
     public function viewContact(){
         $contacts = $this->getDoctrine()->getRepository('AppBundle:contact')->findAll();
         //print_r($contacts);
+
+
+
+        $data = array();
+
+            foreach($contacts as $e)
+            {
+                $data[] =
+                    array(
+                        'id' => $e->getId(),
+                        'fname' => $e->getFname(),
+                        'lname' => $e->getLname(),
+                        'telno' => $e->getEmail(),
+                        'email' => $e->getTelno()
+                    );
+
+
+
+            }
+
+        $response = new Response(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT');
+        $response->headers->set('Access-Control-Allow-Headers', 'X-Header-One,X-Header-Two');
+        return $response;
+
+
+
+
+
         //exit();
-        return $this->render("pages/view.html.twig", ['contacts' => $contacts]);
+        //return $this->render("pages/view.html.twig", ['contacts' => $contacts]);
     }
 
 
     /**
-     * @Route("/post/addcontact", name="add_contact_route")
+     * @Route("/post/deletecontact/{id}", name="delete_contact_route")
      */
-    public function addContact(){
+    public function deleteContact($id){
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('AppBundle:contact')->findOneBy(array('id' => $id));
+
+        if ($entity != null){
+            $em->remove($entity);
+            $em->flush();
+        }
+        $this->addFlash('message','Contact Succcessfully Deleted!');
+        return $this->redirectToRoute('view_contact_route');
+    }
+
+    /**
+     * @Route("/post/updatecontact/{id}", name="update_contact_route")
+     */
+    public function updateContact($id,Request $request){
+        $contact = $this->getDoctrine()->getRepository('AppBundle:contact')->find($id);
+
+        $form = $this->createFormBuilder($contact)
+            ->add('fname', TextType::class, array('attr' =>  array('class' => 'form-control')))
+            ->add('lname', TextType::class, array('attr' =>  array('class' => 'form-control')))
+            ->add('telno', TextType::class, array('attr' =>  array('class' => 'form-control')))
+            ->add('email', EmailType::class, array('attr' =>  array('class' => 'form-control')))
+            ->add('Update', SubmitType::class, array('attr' =>  array('class' => 'btn btn-primary')))
+
+            ->getForm();
+        $form -> handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $fname = $form['fname']->getData();
+            $lname = $form['lname']->getData();
+            $email = $form['email']->getData();
+            $telno = $form['telno']->getData();
+
+
+            $em = $this->getDoctrine()->getManager();
+            $contact = $em->getRepository('AppBundle:contact')->find($id);
+
+            $contact->setFname($fname);
+            $contact->setLname($lname);
+            $contact->setEmail($email);
+            $contact->setTelno($telno);
+
+            $em->flush();
+
+
+            $this->addFlash('message','Contact Succcessfully Updated!');
+            return $this->redirectToRoute('view_contact_route');
+
+        }
+
+
+
+        return $this->render("pages/update.html.twig", [
+            'form' => $form->createView()  ]);
+    }
+
+    /**
+     * @Route("/post/showcontact/{id}", name="show_contact_route")
+     */
+    public function showContact($id){
+
+        $contacts = $this->getDoctrine()->getRepository('AppBundle:contact')->find($id);
+        $fname = $contacts->getFname();
+        $lname = $contacts->getLname();
+        $telno = $contacts->getTelno();
+        $email = $contacts->getEmail();
+
+        $data = [
+                'fname' => $fname,
+                'lname' => $lname,
+                'telno' => $telno,
+                'email' => $email,
+            ];
+        $response = new Response(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+
 
     }
+
 }
